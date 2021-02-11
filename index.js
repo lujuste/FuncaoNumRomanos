84bba3a4116897b14db8d609c96dd5ee4570c26a
Lucas Juste <lu.scatollini@hotmail.com>
2021-02-11 16:06:27 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+function romano2int(romano) {
+    let n = 0;
+    
+    const numeros = {
+        'I': 1,
+        'V': 5,
+        'X': 10, 
+        'L': 50, 
+        'C': 100, 
+        'D': 500,
+        'M': 1000
+    };
+
+    for(let i=0;i<romano.length; i++) {
+        let atual = romano[i];
+        let prox = romano[i+1];
+
+    if(prox && numeros[prox] > numeros[atual]) {
+            n -= numeros[atual];
+        } else {
+            n += numeros[atual];
+        }
+    }
+
+    return n;
+}
+
+
+console.log(romano2int('XX'));
